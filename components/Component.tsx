5eb41814903879083b311bdc5e968e46df588428
Lakori4 <santivalde04@gmail.com>
2025-04-03 20:29:02 +0200

    init

---
+++ components/Component.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent } from "preact/src/index.d.ts";
+
+
+
+
+const Component:FunctionComponent = () => {
+
+  return (
+    <div>Esto es un componente</div>
+  )
+
+
+}
+
+
+export default Component
---
+++ routes/index.tsx
@@ -0,0 +1,16 @@
+import Component from "../components/Component.tsx";
+
+
+const Home = () => {
+
+  return (
+    <div>
+      Estás en casa
+      <Component></Component>
+    </div>
+    
+  )
+
+}
+
+export default Home
